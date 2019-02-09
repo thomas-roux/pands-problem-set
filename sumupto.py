443beed948e3a86d779a3ec4f7f7184deae8abdf
Thomas Roux <thomas.roux@ucdconnect.ie>
2019-02-09 17:02:52 +0000

    Initial solution to problem 1

--- sumupto.py
+++ sumupto.py
@@ -1,4 +1,15 @@
 # Thomas Roux
-# Solution to Problem 1
+# Solution to Problem 1 - calculates sum of all 
+# numbers between one and user entered integer
 # Begun: 09/02/2019; 16h00
 
+# Asks user to enter an integer
+i = int(input("Enter an integer: "))
+
+# Calculates sum and prints answer
+ans = 1
+total = 0
+while ans <= i:
+    total = total + ans
+    ans = ans + 1
+print(total)
