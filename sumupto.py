cad9f48623b7af07c10baab83c955023c7a7383f
Thomas Roux <thomas.roux@ucdconnect.ie>
2019-02-17 21:14:01 +0000

    Added in code to change negative integer to positive; cleaned up output display

--- sumupto.py
+++ sumupto.py
@@ -4,7 +4,15 @@
 # Begun: 09/02/2019; 16h00
 
 # Asks user to enter an integer
-i = int(input("Please enter a positive integer: "))
+i = int(input("\nPlease enter a positive integer: "))
+
+# Checks to see if entered characters a valid integer
+if i == 0:
+    print("Invalid character - please enter an integer above zero.")
+    # need to work out how to exit programme here or loop back to line 7
+elif i < 0:
+    i = abs(i)
+    print("\nNegative integer changed to positive.")
 
 # Calculates sum and prints answer
 ans = 1
@@ -12,4 +20,5 @@ total = 0
 while ans <= i:
     total = total + ans
     ans = ans + 1
-print(total)
+print("\nThe sum of all numbers between 1 and " + str(i) + " is: " + str(total))
+print()
