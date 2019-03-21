1bb8b3bfc0422b65ca755cdb7c9aa54825e9720c
Thomas Roux <thomas.roux@ucdconnect.ie>
2019-03-21 22:43:48 +0000

    Added appropriate suffix to date, todays-date.py. (Kudos to ED, fellow classmate, for the advice on the student forum)

--- todays-date.py
+++ todays-date.py
@@ -5,10 +5,18 @@
 # Imports datetime module
 import datetime as dt
 
+# Creates the necessary suffix for the date based on the date number
+# Kudos to ED (fellow student) for explaining
+suf = ()
+if dt.datetime.strftime(dt.datetime.now(), "%d") == 1 or 21 or 31:
+    suf = ("st")
+elif dt.datetime.strftime(dt.datetime.now(), "%d") == 2 or 22:
+    suf = ("nd")
+elif dt.datetime.strftime(dt.datetime.now(), "%d") == 3 or 23:
+    suf = ("rd")
+else:
+    suf = ("th")
+
 # Prints today's date in specified format
 # dash (-) in %-d and %-I removes zero - https://stackoverflow.com/questions/904928/python-strftime-date-without-leading-0
-print(dt.datetime.strftime(dt.datetime.now(), "%A, %B %-d %Y at %-I:%M %p"))
-
-
-
-# need to come back to work out how to put -st, -rd and -th into day
+print(dt.datetime.strftime(dt.datetime.now(), "%A, %B %-d" + suf + " %Y at %-I:%M %p"))
